185be4a508d75fabf50a2eef2f4fbfce688443ac
Sam Andreae <contact@samandreae.com>
2024-12-07 14:48:37 +0000

    WIP: fix compilation errors

--- src/network.rs
+++ src/network.rs
@@ -6,8 +6,9 @@ use p2panda_net::{NetworkBuilder, SyncConfiguration};
 use p2panda_store::MemoryStore;
 use p2panda_sync::log_sync::LogSyncProtocol;
 use p2panda_sync::{TopicMap, TopicQuery};
+use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, Eq, std::hash::Hash, Serialize, Deserialize)]
+#[derive(Clone, Default, Debug, PartialEq, Eq, std::hash::Hash, Serialize, Deserialize)]
 struct TextDocument(Hash);
 
 impl TopicQuery for TextDocument {}
@@ -31,10 +32,11 @@ impl Extension<PruneFlag> for AarvdarkExtensions {
 
 type LogId = TextDocument;
 
+#[derive(Debug)]
 struct Topic2TextDocument {}
 
 #[async_trait]
-impl TopicMap for Topic2TextDocument {
+impl TopicMap<TextDocument, LogId> for Topic2TextDocument {
     async fn get(&self, topic: &TextDocument) -> Option<LogId> {
         Some(topic.clone())
     }
@@ -46,10 +48,11 @@ pub async fn run() -> Result<()> {
 
     let store = MemoryStore::<LogId, AarvdarkExtensions>::new();
 
-    let sync = LogSync::new(store, topic_map);
+    let topic_map = Topic2TextDocument {};
+    let sync = LogSyncProtocol::new(topic_map, store);
     let sync_config = SyncConfiguration::new(sync);
 
-    let mut network = NetworkBuilder::new(network_id.as_bytes())
+    let mut network = NetworkBuilder::new(*network_id.as_bytes())
         .sync(sync_config)
         .private_key(private_key)
         .discovery(LocalDiscovery::new()?)
@@ -57,7 +60,7 @@ pub async fn run() -> Result<()> {
         .await?;
 
     let test_document = TextDocument(Hash::new(b"my first doc <3"));
-    let (tx, rx, ready) = network.subscribe(topic).await?;
+    let (tx, rx, ready) = network.subscribe(test_document).await?;
 
     Ok(())
 }
