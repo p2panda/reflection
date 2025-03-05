1fa18b23cc68514f9a7fcd351338cc2a7394474e
Julian Sparber <julian@sparber.net>
2025-03-05 18:36:14 +0100

    app: Multiline entry for entering document id when opening
    This adds a multiline entry to the document open dialog. It allows only
    allows single spaces and max 64 hex digits

---
+++ aardvark-app/src/components/multiline_entry.rs
@@ -0,0 +1,51 @@
+use gtk::{glib, subclass::prelude::*, prelude::*, gdk};
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    pub struct MultilineEntry {}
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for MultilineEntry {
+        const NAME: &'static str = "MultilineEntry";
+        type Type = super::MultilineEntry;
+        type ParentType = gtk::TextView;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.set_css_name("entry");
+        }
+    }
+
+    impl ObjectImpl for MultilineEntry {
+        fn constructed (&self) {
+            let key_events = gtk::EventControllerKey::new();
+            key_events.connect_key_pressed(|controller, key, _, modifier| {
+                    if modifier.is_empty() && (key == gdk::Key::Return || key == gdk::Key::KP_Enter)
+                    {
+                        if let Some(widget) = controller.widget() {
+                            widget.activate_default();
+                        }
+                        glib::Propagation::Stop
+                    } else {
+                        glib::Propagation::Proceed
+                    }
+                }
+            );
+            self.obj().add_controller(key_events);
+        }
+    }
+    impl WidgetImpl for MultilineEntry {}
+    impl TextViewImpl for MultilineEntry {}
+}
+
+glib::wrapper! {
+    pub struct MultilineEntry(ObjectSubclass<imp::MultilineEntry>)
+        @extends gtk::Widget, gtk::TextView;
+}
+
+impl MultilineEntry {
+    pub fn new() -> Self {
+        glib::Object::new()
+    }
+}
