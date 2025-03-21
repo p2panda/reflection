ef92f4fe599a1dae79b6dda6aa76448dfc803dd5
Julian Sparber <julian@sparber.net>
2025-03-21 13:23:31 +0100

    chore: Add config.rs to gitignore

--- aardvark-app/src/config.rs
+++
@@ -1,5 +0,0 @@
-pub static APP_ID: &str = "org.p2panda.aardvark";
-pub static VERSION: &str = "0.1.0";
-pub static GETTEXT_PACKAGE: &str = "aardvark";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/aardvark";
