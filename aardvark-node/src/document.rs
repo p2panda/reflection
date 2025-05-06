3a7ef4e04491c021172a47df54c110c080d7193e
Julian Sparber <julian@sparber.net>
2025-05-06 19:32:26 +0200

    node: Serialize and deserialize documents for sqlx
    This add types and implements traits needed to write and read documents
    and authors to and from the sqlite DB.

--- aardvark-node/src/document.rs
+++ aardvark-node/src/document.rs
@@ -2,13 +2,26 @@ use std::fmt;
 use std::hash::Hash as StdHash;
 use std::str::FromStr;
 
+use chrono::{DateTime, Utc};
 use p2panda_core::{Hash, HashError, PublicKey};
 use p2panda_net::TopicId;
 use p2panda_sync::TopicQuery;
 use serde::{Deserialize, Serialize};
+use sqlx::{
+    Decode, Encode, FromRow, Sqlite, Type,
+    encode::IsNull,
+    error::BoxDynError,
+    sqlite::{SqliteArgumentValue, SqliteTypeInfo, SqliteValueRef},
+};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, StdHash, Serialize, Deserialize)]
 pub struct DocumentId(Hash);
+
+impl DocumentId {
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
 
 impl TopicQuery for DocumentId {}
 
@@ -54,6 +67,58 @@ impl FromStr for DocumentId {
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         Ok(Hash::from_str(value)?.into())
     }
+}
+
+impl TryFrom<&[u8]> for DocumentId {
+    type Error = HashError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        Ok(Hash::try_from(value)?.into())
+    }
+}
+
+impl Type<Sqlite> for DocumentId {
+    fn type_info() -> SqliteTypeInfo {
+        <&[u8] as Type<Sqlite>>::type_info()
+    }
+
+    fn compatible(ty: &SqliteTypeInfo) -> bool {
+        <&[u8] as Type<Sqlite>>::compatible(ty)
+    }
+}
+
+impl<'q> Encode<'q, Sqlite> for &'q DocumentId {
+    fn encode_by_ref(
+        &self,
+        args: &mut Vec<SqliteArgumentValue<'q>>,
+    ) -> Result<IsNull, BoxDynError> {
+        <&[u8] as Encode<Sqlite>>::encode_by_ref(&self.as_bytes(), args)
+    }
+}
+
+impl Decode<'_, Sqlite> for DocumentId {
+    fn decode(value: SqliteValueRef<'_>) -> Result<Self, BoxDynError> {
+        Ok(DocumentId::try_from(<&[u8] as Decode<Sqlite>>::decode(
+            value,
+        )?)?)
+    }
+}
+
+#[derive(Debug, FromRow)]
+pub struct Document {
+    #[sqlx(rename = "document_id")]
+    pub id: DocumentId,
+    #[sqlx(default)]
+    pub name: Option<String>,
+    pub last_accessed: Option<DateTime<Utc>>,
+    #[sqlx(skip)]
+    pub authors: Vec<Author>,
+}
+
+#[derive(Debug)]
+pub struct Author {
+    pub public_key: PublicKey,
+    pub last_seen: Option<DateTime<Utc>>,
 }
 
 pub trait SubscribableDocument: Sync + Send {
