3362ef2d3ce7fd7c1d3445cf2fad2f10b2eb8420
cafca <cafca@001.land>
2025-05-26 00:33:09 +0200

    Add macOS get_or_create_identity() variant

--- aardvark-app/src/secret.rs
+++ aardvark-app/src/secret.rs
@@ -31,8 +31,12 @@ fn attributes() -> HashMap<&'static str, String> {
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[cfg(not(target_os = "macos"))]
     #[error("Secret Service error: {0}")]
     Service(oo7::Error),
+    #[cfg(target_os = "macos")]
+    #[error("Keyring error: {0}")]
+    Service(keyring::Error),
     #[error("Format error: {0}")]
     Format(IdentityError),
 }
@@ -43,12 +47,14 @@ impl From<IdentityError> for Error {
     }
 }
 
+#[cfg(not(target_os = "macos"))]
 impl From<oo7::Error> for Error {
     fn from(value: oo7::Error) -> Self {
         Error::Service(value)
     }
 }
 
+#[cfg(not(target_os = "macos"))]
 pub async fn get_or_create_identity() -> Result<PrivateKey, Error> {
     let keyring = oo7::Keyring::new().await?;
 
@@ -73,6 +79,33 @@ pub async fn get_or_create_identity() -> Result<PrivateKey, Error> {
             );
             private_key
         };
+
+    Ok(private_key)
+}
+
+#[cfg(target_os = "macos")]
+pub async fn get_or_create_identity() -> Result<PrivateKey, Error> {
+    let entry = keyring::Entry::new("Aardvark Identity", "default user").map_err(Error::Service)?;
+
+    let private_key: PrivateKey = match entry.get_password() {
+        Ok(password) => {
+            let private_key = PrivateKey::try_from(password.as_bytes())?;
+            info!("Found existing identity: {}", private_key.public_key());
+            private_key
+        }
+        Err(keyring::Error::NoEntry) => {
+            let private_key = PrivateKey::new();
+            entry
+                .set_password(&String::from_utf8_lossy(private_key.as_bytes()))
+                .map_err(Error::Service)?;
+            info!(
+                "No existing identity found. Create new identity: {}",
+                private_key.public_key()
+            );
+            private_key
+        }
+        Err(e) => return Err(Error::Service(e)),
+    };
 
     Ok(private_key)
 }
