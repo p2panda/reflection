34047ecf2568def46df8e257c1a35fcb8d0ba3ac
Julian Sparber <julian@sparber.net>
2025-08-22 17:29:37 +0200

    doc: Adjust tests to API changes

--- reflection-doc/src/lib.rs
+++ reflection-doc/src/lib.rs
@@ -72,150 +72,139 @@ pub mod identity {
 
 #[cfg(test)]
 mod tests {
+    use glib::clone;
+    use test_log::test;
+
     use crate::document::Document;
     use crate::identity::PrivateKey;
     use crate::service::Service;
-    use gio::prelude::FileExt;
-    use glib::object::ObjectExt;
-    use std::fs;
-    use test_log::test;
-
-    struct TestResource {
-        service: Service,
-    }
-
-    impl Drop for TestResource {
-        fn drop(&mut self) {
-            if let Some(data_dir) = self.service.data_dir() {
-                fs::remove_dir_all(data_dir.path().unwrap()).expect("Able to remove data dir");
-            }
-        }
-    }
-
-    impl TestResource {
-        /// Creates a new `TestResource` that includes the `Service`
-        fn new() -> TestResource {
-            let private_key = PrivateKey::new();
-            let mut data_path = glib::tmp_dir();
-            data_path.push("Reflection");
-            data_path.push(private_key.public_key().to_string());
-            fs::create_dir_all(&data_path).expect("Able to create data dir");
-            let data_dir = gio::File::for_path(data_path);
-
-            TestResource {
-                service: Service::new(&private_key, Some(&data_dir)),
-            }
-        }
-
-        fn service(&self) -> Service {
-            self.service.clone()
-        }
-    }
 
     #[test]
     fn create_document() {
-        let context = glib::MainContext::default();
-
-        let resource = TestResource::new();
-        let service = resource.service();
         let test_string = "Hello World";
-        glib::MainContext::new()
-            .block_on(async { service.startup().await })
-            .unwrap();
-        let document = Document::new(&service, None);
-        document.set_subscribed(true);
-        context.iteration(false);
-        assert!(document.insert_text(0, test_string).is_ok());
-        assert_eq!(document.text(), test_string);
+
+        let context = glib::MainContext::new();
+        let main_loop = glib::MainLoop::new(Some(&context), false);
+
+        context.spawn_local(clone!(
+            #[strong]
+            context,
+            #[strong]
+            main_loop,
+            async move {
+                let private_key = PrivateKey::new();
+                let service = Service::new(&private_key, None);
+                service.startup().await.unwrap();
+
+                let document = Document::create_with_main_context(&service, &context).await;
+                document.subscribe().await;
+
+                assert!(document.insert_text(0, test_string).is_ok());
+                assert_eq!(document.text(), test_string);
+
+                service.shutdown().await;
+                main_loop.quit();
+            }
+        ));
+
+        main_loop.run();
     }
 
     #[test]
     fn basic_sync() {
-        let main_loop = glib::MainLoop::new(None, false);
         let test_string = "Hello World";
-        let resource = TestResource::new();
-        let service = resource.service();
-        glib::MainContext::new()
-            .block_on(async { service.startup().await })
-            .unwrap();
 
-        let document = Document::new(&service, None);
-        document.set_subscribed(true);
-        let id = document.id();
+        let context = glib::MainContext::new();
+        let main_loop = glib::MainLoop::new(Some(&context), false);
 
-        let resource2 = TestResource::new();
-        let service2 = resource2.service();
-        glib::MainContext::new()
-            .block_on(async { service2.startup().await })
-            .unwrap();
-        let document2 = Document::new(&service2, Some(&id));
-        document2.set_subscribed(true);
+        context.spawn_local(clone!(
+            #[strong]
+            context,
+            #[strong]
+            main_loop,
+            async move {
+                let private_key = PrivateKey::new();
+                let service = Service::new(&private_key, None);
+                service.startup().await.unwrap();
 
-        assert_eq!(document.id(), document2.id());
-        main_loop.context().spawn(async move {
-            assert!(document.insert_text(0, test_string).is_ok());
-            assert_eq!(document.text(), test_string);
-        });
+                let document = Document::create_with_main_context(&service, &context).await;
+                document.subscribe().await;
+                let id = document.id();
 
-        let main_loop_clone = main_loop.clone();
-        document2.connect_notify(Some("text"), move |_, _| {
-            main_loop_clone.quit();
-        });
+                let private_key2 = PrivateKey::new();
+                let service2 = Service::new(&private_key2, None);
+                service2.startup().await.unwrap();
+
+                let document2 = Document::with_main_context(&service2, &id, &context);
+                document2.subscribe().await;
+
+                assert_eq!(document.id(), document2.id());
+
+                assert!(document.insert_text(0, test_string).is_ok());
+                assert_eq!(document.text(), test_string);
+
+                service.shutdown().await;
+                service2.shutdown().await;
+
+                assert_eq!(document2.text(), test_string);
+
+                main_loop.quit();
+            }
+        ));
 
         main_loop.run();
-        service.shutdown();
-        service2.shutdown();
-
-        assert_eq!(document2.text(), test_string);
     }
 
     #[test]
     fn sync_multiple_changes() {
-        let main_loop = glib::MainLoop::new(None, false);
         let expected_string = "Hello, World!";
-        let resource = TestResource::new();
-        let service = resource.service();
-        glib::MainContext::new()
-            .block_on(async { service.startup().await })
-            .unwrap();
 
-        let document = Document::new(&service, None);
-        document.set_subscribed(true);
-        let id = document.id();
+        let context = glib::MainContext::new();
+        let main_loop = glib::MainLoop::new(Some(&context), false);
 
-        let resource2 = TestResource::new();
-        let service2 = resource2.service();
-        glib::MainContext::new()
-            .block_on(async { service2.startup().await })
-            .unwrap();
-        let document2 = Document::new(&service2, Some(&id));
-        document2.set_subscribed(true);
+        context.spawn_local(clone!(
+            #[strong]
+            context,
+            #[strong]
+            main_loop,
+            async move {
+                let private_key = PrivateKey::new();
+                let service = Service::new(&private_key, None);
+                service.startup().await.unwrap();
 
-        assert_eq!(document.id(), document2.id());
-        main_loop.context().spawn(async move {
-            assert!(document.insert_text(0, "Hello,").is_ok());
-            assert!(document.insert_text(6, " World!").is_ok());
-            assert!(document.delete_range(7, 8).is_ok());
-            assert!(document.insert_text(7, "W").is_ok());
-            assert_eq!(document.text(), expected_string);
-        });
+                let document = Document::create_with_main_context(&service, &context).await;
+                document.subscribe().await;
+                let id = document.id();
 
-        let main_loop_clone = main_loop.clone();
-        document2.connect_notify(Some("text"), move |document2, _| {
-            if document2.text() == expected_string {
-                main_loop_clone.quit();
+                let private_key2 = PrivateKey::new();
+                let service2 = Service::new(&private_key2, None);
+                service2.startup().await.unwrap();
+
+                let document2 = Document::with_main_context(&service2, &id, &context);
+                document2.subscribe().await;
+
+                assert_eq!(document.id(), document2.id());
+
+                assert!(document.insert_text(0, "Hello,").is_ok());
+                assert!(document.insert_text(6, " World!").is_ok());
+                assert!(document.delete_range(7, 8).is_ok());
+                assert!(document.insert_text(7, "W").is_ok());
+                assert_eq!(document.text(), expected_string);
+
+                service.shutdown().await;
+                service2.shutdown().await;
+
+                assert_eq!(document2.text(), expected_string);
+
+                main_loop.quit();
             }
-        });
+        ));
 
         main_loop.run();
-
-        service.shutdown();
-        service2.shutdown();
     }
 
     #[test]
     fn sync_longer_text() {
-        let main_loop = glib::MainLoop::new(None, false);
         let test_string = "Et aut omnis eos corporis ut. Qui est blanditiis blanditiis.
         Sit quia nam maxime accusantium ut voluptatem. Fuga consequuntur animi et et est.
         Unde voluptas consequatur mollitia id odit optio harum sint. Fugit quo aut et laborum aut cupiditate.";
@@ -223,42 +212,48 @@ mod tests {
             "{}{}{}{}",
             test_string, test_string, test_string, test_string
         );
-        let resource = TestResource::new();
-        let service = resource.service();
-        glib::MainContext::new()
-            .block_on(async { service.startup().await })
-            .unwrap();
 
-        let document = Document::new(&service, None);
-        document.set_subscribed(true);
-        let id = document.id();
+        let context = glib::MainContext::new();
+        let main_loop = glib::MainLoop::new(Some(&context), false);
 
-        let resource2 = TestResource::new();
-        let service2 = resource2.service();
-        glib::MainContext::new()
-            .block_on(async { service2.startup().await })
-            .unwrap();
-        let document2 = Document::new(&service2, Some(&id));
-        document2.set_subscribed(true);
+        context.spawn_local(clone!(
+            #[strong]
+            context,
+            #[strong]
+            main_loop,
+            async move {
+                let private_key = PrivateKey::new();
+                let service = Service::new(&private_key, None);
+                service.startup().await.unwrap();
 
-        assert_eq!(document.id(), document2.id());
-        main_loop.context().spawn(async move {
-            assert!(document.insert_text(0, test_string).is_ok());
-            assert!(document.insert_text(0, test_string).is_ok());
-            assert!(document.insert_text(0, test_string).is_ok());
-            assert!(document.insert_text(0, test_string).is_ok());
-        });
+                let document = Document::create_with_main_context(&service, &context).await;
+                let id = document.id();
 
-        let main_loop_clone = main_loop.clone();
-        document2.connect_notify(Some("text"), move |document2, _| {
-            if document2.text() == expected_string {
-                main_loop_clone.quit();
+                document.subscribe().await;
+
+                let private_key2 = PrivateKey::new();
+                let service2 = Service::new(&private_key2, None);
+                service2.startup().await.unwrap();
+
+                let document2 = Document::with_main_context(&service2, &id, &context);
+                document2.subscribe().await;
+
+                assert_eq!(document.id(), document2.id());
+
+                assert!(document.insert_text(0, test_string).is_ok());
+                assert!(document.insert_text(0, test_string).is_ok());
+                assert!(document.insert_text(0, test_string).is_ok());
+                assert!(document.insert_text(0, test_string).is_ok());
+
+                service.shutdown().await;
+                service2.shutdown().await;
+
+                assert_eq!(document2.text(), expected_string);
+
+                main_loop.quit();
             }
-        });
+        ));
 
         main_loop.run();
-
-        service.shutdown();
-        service2.shutdown();
     }
 }
