8cdd4498241af0fb144a78da54073d817e1a37a5
Julian Sparber <julian@sparber.net>
2025-12-09 12:01:02 +0100

    app: Add component to display an status indicator
    This will be used for the online status dot of authors.

--- reflection-app/src/components/mod.rs
+++ reflection-app/src/components/mod.rs
@@ -1,7 +1,9 @@
 mod avatar;
+mod indicator_bin;
 mod multiline_entry;
 mod zoom_level_selector;
 
 pub use self::avatar::Avatar;
+pub use self::indicator_bin::IndicatorBin;
 pub use self::multiline_entry::MultilineEntry;
 pub use self::zoom_level_selector::ZoomLevelSelector;
