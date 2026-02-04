25787c42b258aed20fa12ea4892c116dcc8f350b
Julian Sparber <julian@sparber.net>
2026-02-04 14:39:12 +0100

    app: Allow app to build on MacOS
    This adds code and dependency needed for MacOS, using conditional
    compilation.

--- reflection-app/src/secret.rs
+++ reflection-app/src/secret.rs
@@ -16,27 +16,42 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#[cfg(target_os = "linux")]
 use std::collections::HashMap;
 use thiserror::Error;
 use tracing::info;
 
+#[cfg(target_os = "linux")]
 use crate::APP_ID;
 use reflection_doc::identity::{IdentityError, PrivateKey};
 
-const XDG_SCHEMA: &str = "xdg:schema";
+#[cfg(target_os = "linux")]
+const XDG_SCHEMA: &'static str = "xdg:schema";
 
+#[cfg(target_os = "linux")]
 fn attributes() -> HashMap<&'static str, String> {
     HashMap::from([(XDG_SCHEMA, APP_ID.to_owned())])
 }
 
+#[cfg(target_os = "macos")]
+use base64::engine::general_purpose::STANDARD as Base64Engine;
+
+#[cfg(target_os = "macos")]
+use base64::Engine as _;
+
 #[derive(Debug, Error)]
 pub enum Error {
+    #[cfg(target_os = "linux")]
     #[error(transparent)]
     Service(#[from] oo7::Error),
+    #[cfg(target_os = "macos")]
+    #[error(transparent)]
+    Service(#[from] keyring::Error),
     #[error(transparent)]
     Format(#[from] IdentityError),
 }
 
+#[cfg(target_os = "linux")]
 pub async fn get_or_create_identity() -> Result<PrivateKey, Error> {
     let keyring = oo7::Keyring::new().await?;
 
@@ -61,6 +76,36 @@ pub async fn get_or_create_identity() -> Result<PrivateKey, Error> {
             );
             private_key
         };
+
+    Ok(private_key)
+}
+
+#[cfg(target_os = "macos")]
+pub async fn get_or_create_identity() -> Result<PrivateKey, Error> {
+    let entry = keyring::Entry::new("Reflection Identity", "default user")?;
+
+    let private_key: PrivateKey = match entry.get_password() {
+        Ok(password) => {
+            let private_key = PrivateKey::try_from(
+                Base64Engine
+                    .decode(password)
+                    .expect("Failed to decode base64 secret from keyring")
+                    .as_slice(),
+            )?;
+            info!("Found existing identity: {}", private_key.public_key());
+            private_key
+        }
+        Err(keyring::Error::NoEntry) => {
+            let private_key = PrivateKey::new();
+            entry.set_password(&Base64Engine.encode(private_key.as_bytes()))?;
+            info!(
+                "No existing identity found. Create new identity: {}",
+                private_key.public_key()
+            );
+            private_key
+        }
+        Err(e) => return Err(Error::Service(e)),
+    };
 
     Ok(private_key)
 }
