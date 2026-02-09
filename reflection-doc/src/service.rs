90a95b9043fc27e6e0b73f0b8f2372d70c6a3122
Julian Sparber <julian@sparber.net>
2026-02-09 20:26:32 +0100

    node: Set connection mode always after constructing the node
    This allows to report network errors on the set_connection_mode(), but
    creating a node will always succeed.

--- reflection-doc/src/service.rs
+++ reflection-doc/src/service.rs
@@ -174,15 +174,7 @@ impl Service {
         let private_key = self.private_key().0;
         let network_id = Hash::new(b"reflection");
         let path = self.data_dir().and_then(|data_dir| data_dir.path());
-        let node = Node::new(
-            private_key,
-            network_id,
-            path.as_deref(),
-            // gio::NetworkManager is slow to initialize the `network-available` property,
-            // so it might be incorrect therefore always start with no connection.
-            node::ConnectionMode::None,
-        )
-        .await?;
+        let node = Node::new(private_key, network_id, path.as_deref()).await?;
 
         self.imp()
             .node
