a0e871273697b076d42c460931ac5735ebcc261f
adz <x12@adz.garden>
2026-03-18 16:57:40 +0100

    node: Update tests after API changes

--- reflection-node/src/lib.rs
+++ reflection-node/src/lib.rs
@@ -13,9 +13,8 @@ mod tests {
     use p2panda_core::{Hash, SigningKey, Topic, VerifyingKey};
     use tokio::sync::{Mutex, mpsc};
 
-    use crate::node::ConnectionMode;
-    use crate::node::Node;
-    use crate::topic::SubscribableTopic;
+    use crate::node::{ConnectionMode, Node};
+    use crate::traits::{SubscribableTopic, SubscriptionError};
 
     #[tokio::test]
     #[test_log::test]
@@ -26,10 +25,10 @@ mod tests {
 
         let id: [u8; 32] = [0; 32];
         let _sub = node.subscribe(id, TestTopic::new()).await;
-        let topics = node.topics::<[u8; 32]>().await.unwrap();
+        let topics = node.topics().await.unwrap();
 
         assert_eq!(topics.len(), 1);
-        assert_eq!(topics.first().unwrap().id, id);
+        assert_eq!(topics.first().unwrap().id, id.into());
 
         node.shutdown().await.unwrap();
     }
@@ -80,16 +79,11 @@ mod tests {
 
         let test_topic = TestTopic::new();
 
-        let id: [u8; 32] = [0; 32];
-        let subscription = node.subscribe(id, test_topic).await.unwrap();
+        let subscription = node.subscribe(topic_id, test_topic).await.unwrap();
 
-        let topics = node.topics::<[u8; 32]>().await.unwrap();
-        assert_eq!(topics.len(), 1);
-        assert_eq!(topics.first().unwrap().id, id);
-
-        let private_key2 = PrivateKey::new();
-        let network_id2 = Hash::new(b"reflection");
-        let node2 = Node::new(private_key2, network_id2, None).await.unwrap();
+        let node2 = Node::new(SigningKey::generate(), network_id, None)
+            .await
+            .unwrap();
         node2
             .set_connection_mode(ConnectionMode::Network)
             .await
@@ -97,15 +91,17 @@ mod tests {
 
         let test_topic2 = TestTopic::new();
 
-        let _subscription2 = node2.subscribe(id, test_topic2.clone()).await.unwrap();
+        let _subscription2 = node2
+            .subscribe(topic_id, test_topic2.clone())
+            .await
+            .unwrap();
 
-        let topics2 = node2.topics::<[u8; 32]>().await.unwrap();
-        assert_eq!(topics2.len(), 1);
-        assert_eq!(topics2.first().unwrap().id, id);
+        // TODO: Need to sleep here to make sure tx already exists.
+        tokio::time::sleep(std::time::Duration::from_secs(3)).await;
 
         let test_snapshot = "test".as_bytes().to_vec();
         subscription
-            .send_snapshot(test_snapshot.clone())
+            .publish_snapshot(test_snapshot.clone())
             .await
             .unwrap();
 
